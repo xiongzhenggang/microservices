abfee34687fbbe74a5b6c403618be31412001add
xiongzhenggang <ab1290014555@qq.com>
2020-02-12 18:34:31 +0800

    demo sql

---
+++ microservices-platform/zlt-doc/sql/demo-sql/user-center.sql
@@ -0,0 +1,247 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : discuz
+Source Server Version : 50644
+Source Host           : 118.24.61.199:3306
+Source Database       : user-center
+
+Target Server Type    : MYSQL
+Target Server Version : 50644
+File Encoding         : 65001
+Date: 2020-02-12 16:23:04
+*/
+
+
+CREATE DATABASE IF NOT EXISTS `auth-center` DEFAULT CHARACTER SET = utf8;
+Use `auth-center`;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `mobile` varchar(11) DEFAULT NULL,
+  `password` varchar(60) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nick_name` varchar(255) DEFAULT NULL,
+  `photo` varchar(1024) DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `type` varchar(16) NOT NULL,
+  `create_user` varchar(255) DEFAULT NULL,
+  `update_user` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `open_id` varchar(32) DEFAULT NULL,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_username` (`user_name`),
+  KEY `idx_mobile` (`mobile`),
+  KEY `idx_open_id` (`open_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', 'admin', '1832322322', '$2a$10$TJkwVdlpbHKnV45.nBxbgeFHmQRmyWlshg94lFu2rKxVtT2OMniDO', '163@163.com', '管理员', null, '1', 'APP', null, null, '2020-02-12 16:55:31', null, '123', '0');
+
+
+-- ----------------------------
+-- Table structure for sys_role_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role_rel`;
+CREATE TABLE `sys_user_role_rel` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `role_id` int(11) NOT NULL,
+    `create_user` varchar(255) DEFAULT NULL,
+    `update_user` varchar(255) DEFAULT NULL,
+    `create_time` datetime DEFAULT NULL,
+    `update_time` datetime DEFAULT NULL,
+    `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`),
+   KEY `idx_role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of sys_role_user
+-- ----------------------------
+INSERT INTO `sys_user_role_rel` VALUES ('1', '1', '1', null, null, '2020-02-12 17:08:12', '2020-02-12 17:08:14', '0');
+
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(32) NOT NULL COMMENT '角色code',
+  `name` varchar(50) NOT NULL COMMENT '角色名',
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `create_user` varchar(255) DEFAULT NULL,
+  `update_user` varchar(255) DEFAULT NULL,
+  `type` varchar(16) NOT NULL,
+  `tenant_id` varchar(32) DEFAULT '' COMMENT '租户字段',
+  PRIMARY KEY (`id`),
+  KEY `idx_code` (`code`),
+  KEY `idx_type` (`type`),
+  KEY `idx_tenant_id` (`tenant_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', 'ADMIN', 'admin', '2020-02-12 17:32:00', '2020-02-12 17:32:03', null, null, 'app', '1');
+
+
+-- ----------------------------
+-- Table structure for sys_role_menu_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu_rel`;
+CREATE TABLE `sys_role_menu_rel` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `create_user` varchar(255) DEFAULT NULL,
+  `update_user` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+   `update_time` datetime DEFAULT NULL,
+   `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`),
+  KEY `idx_role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of sys_role_menu_rel
+-- ----------------------------
+INSERT INTO `sys_role_menu_rel` VALUES ('1', '1', '1', null, null, null, null, '0');
+INSERT INTO `sys_role_menu_rel` VALUES ('2', '1', '2', null, null, null, null, '0');
+INSERT INTO `sys_role_menu_rel` VALUES ('3', '1', '3', null, null, null, null, '0');
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `menu_route` varchar(1024) DEFAULT NULL COMMENT '前端路由id',
+  `url` varchar(1024) DEFAULT NULL COMMENT 'url资源信息',
+  `method` varchar(10) DEFAULT NULL COMMENT '请求方法',
+  `icon` varchar(1024) DEFAULT NULL ,
+  `sort` int(11) NOT NULL,
+  `type` varchar(16) NOT NULL COMMENT 'menu:菜单 resource 资源信息',
+  `describe` varchar(64) DEFAULT NULL,
+  `create_user` varchar(255) DEFAULT NULL,
+  `update_user` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `tenant_id` varchar(32) DEFAULT '' COMMENT '租户字段',
+  PRIMARY KEY (`id`),
+  KEY `idx_parent_id` (`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_org`;
+CREATE TABLE `sys_org` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `icon` varchar(1024) DEFAULT NULL ,
+  `sort` int(11) NOT NULL,
+  `type` varchar(16) NOT NULL COMMENT 'menu:菜单 resource 资源信息',
+  `describe` varchar(64) DEFAULT NULL,
+  `create_user` varchar(255) DEFAULT NULL,
+  `update_user` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_parent_id` (`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sys_org_role_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_org_role_rel`;
+CREATE TABLE `sys_org_role_rel` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+  `org_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `create_user` varchar(255) DEFAULT NULL,
+  `update_user` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+   `update_time` datetime DEFAULT NULL,
+   `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sys_org_user_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_org_user_rel`;
+CREATE TABLE `sys_org_user_rel` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+  `org_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `create_user` varchar(255) DEFAULT NULL,
+  `update_user` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+   `update_time` datetime DEFAULT NULL,
+   `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sys_org_auth_data_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_org_auth_data_rel`;
+CREATE TABLE `sys_org_auth_data_rel` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+  `org_id` int(11) NOT NULL,
+  `auth_data_id` int(11) NOT NULL,
+  `create_user` varchar(255) DEFAULT NULL,
+  `update_user` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+   `update_time` datetime DEFAULT NULL,
+   `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sys_auth_data
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_auth_data`;
+CREATE TABLE `sys_auth_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+   `parent_id` int(11) NOT NULL,
+   `name` varchar(64) NOT NULL,
+   `icon` varchar(1024) DEFAULT NULL ,
+   `sort` int(11) NOT NULL,
+   `type` varchar(16) NOT NULL COMMENT '类型',
+   `describe` varchar(64) DEFAULT NULL,
+   `create_user` varchar(255) DEFAULT NULL,
+   `update_user` varchar(255) DEFAULT NULL,
+   `create_time` datetime DEFAULT NULL,
+   `update_time` datetime DEFAULT NULL,
+   `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+
+SET FOREIGN_KEY_CHECKS=1;
