a2c3898d9ec911a2f5a5c4375bc7ea4aa747cf4e
xiongzhenggang <ab1290014555@qq.com>
2020-02-13 09:47:49 +0800

    code gentor

--- microservices-platform/zlt-business/code-generator/src/main/java/com/central/generator/CodeGeneratorApp.java
+++ microservices-platform/zlt-business/code-generator/src/main/java/com/central/generator/CodeGeneratorApp.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class CodeGeneratorApp {
+public class CodeGeneratorApp  {
     public static void main(String[] args) {
         SpringApplication.run(CodeGeneratorApp.class, args);
     }
