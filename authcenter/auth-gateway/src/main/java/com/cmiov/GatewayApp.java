231461ecc9645d68581c4298a904093abf870c2c
wochenmo <13006363930@163.com>
2020-02-20 10:34:10 +0800

    test

--- authcenter/auth-gateway/src/main/java/com/cmiov/GatewayApp.java
+++ authcenter/auth-gateway/src/main/java/com/cmiov/GatewayApp.java
@@ -16,5 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
+
+        
     }
 }
